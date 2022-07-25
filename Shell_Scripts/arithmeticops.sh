eb716341b13c6a44757d57c704cb8084648a4dbc
SriramaBhat <108423390+SriramaBhat@users.noreply.github.com>
2022-07-25 21:50:22 +0530

    Update arithmeticops.sh

--- Shell_Scripts/arithmeticops.sh
+++ Shell_Scripts/arithmeticops.sh
@@ -14,32 +14,43 @@ do
     read choice 
     case "${choice}" in
         1)
-            res=$num1+$num2
+            res=$((num1+num2))
             operation=Addition
         ;;
         2)
-            res=$num1-$num2
+            res=$((num1-num2))
             operation=Subtraction
         ;;
         3)
-            res=$num1*$num2
+            res=$((num1*num2))
             operation=Multiplication
         ;;
         4)
-            res=$num1/$num2
-            operation=Division
+            if [[ $num2 == 0 ]]
+            then
+                echo "Cannot divide by 0"
+                flag=1
+            else 
+                res=$((num1/num2))
+                operation=Division
+            fi
         ;;
         5) 
-            res=$num1%$num2
+            res=$((num1%num2))
             operation=Modulo
         ;;
         6)
+            echo "Thank you"
             break
         ;;
         *)
             echo "Wrong Operation chosen, enter again"
         ;;
     esac
-
+    if [[ $flag == 1 ]]
+    then
+        flag=0 
+        continue
+    fi
     echo "The result of the $operation operation is $res"
 done
