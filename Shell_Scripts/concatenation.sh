e88e46f1f471112d8d9edcb6fa27876ac08beb2a
Srirama Bhat <108423390+SriramaBhat@users.noreply.github.com>
2022-08-01 21:34:10 +0530

    Update concatenation.sh

--- Shell_Scripts/concatenation.sh
+++ Shell_Scripts/concatenation.sh
@@ -3,6 +3,6 @@
 echo "Enter the two strings to be concatenated: "
 read str1
 read str2
-res=$str1$str2
+res=$str1$str2  #Spaces are not considered, therefore use underscore to denote space
 echo "The resultant string is: "
 echo $res  
