611f180665fcf53d16856ae97dfe18e88ed5dada
Srirama Bhat <108423390+SriramaBhat@users.noreply.github.com>
2022-08-01 23:01:39 +0530

    Update largest_of_n.sh

--- Shell_Scripts/largest_of_n.sh
+++ Shell_Scripts/largest_of_n.sh
@@ -2,7 +2,8 @@
 # Program to find the largest of the given array of numbers
 echo "Enter the size of the array: "
 read n
-declare -A arr 
+declare -A arr
+echo "Enter the elements of the array: " 
 for ((i=0; i<n; i++))
 do
     read arr["$i"]
